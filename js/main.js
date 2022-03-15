8970810710fa1c7c74a121605820710d8324fc6a
mharif010 <ccnarif8741@gmail.com>
2022-03-15 18:40:38 +0600

    finally done

--- js/main.js
+++ js/main.js
@@ -139,7 +139,13 @@
           menu.initialize();
   
       });
-  
+
+      $('ul.menu li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+        }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+        });
+        
   
   
   /*------------------------------------------------------------------------------*/
